87575944174c27be5b400db7c893021a480338be
Nikita Berezyuk ☭ <96497560+berezucc@users.noreply.github.com>
2024-07-10 21:00:55 -0400

    Update db.py

--- db/db.py
+++ db/db.py
@@ -1,24 +1,47 @@
 import psycopg2
 import streamlit as st
-import pandas as pd
-from st_supabase_connection import SupabaseConnection
 
-# Initialize the Supabase client
+# Function to insert data into PostgreSQL (Supabase) table
 @st.cache_data(ttl=600)
-def init_supabase_client() -> Client:
-    url = st.secrets["connections"]["supabase"]["SUPABASE_URL"]
-    key = st.secrets["connections"]["supabase"]["SUPABASE_KEY"]
-    return create_client(url, key)
-
-supabase = init_supabase_client()
-
-# Function to insert data into Supabase table
 def insert_input_into_db(table_name, _params):
-    data = supabase.table(table_name).insert(_params).execute()
-    return data
+    connection = psycopg2.connect(
+        host=st.secrets["connections"]["supabase"]["host"],
+        user=st.secrets["connections"]["supabase"]["user"],
+        password=st.secrets["connections"]["supabase"]["password"],
+        database=st.secrets["connections"]["supabase"]["database"],
+        port=st.secrets["connections"]["supabase"]["port"]
+    )
+    cursor = connection.cursor()
+
+    columns = ', '.join(_params.keys())
+    placeholders = ', '.join(['%s'] * len(_params))
+    query = f"INSERT INTO {table_name} ({columns}) VALUES ({placeholders})"
+
+    cursor.execute(query, tuple(_params.values()))
+    connection.commit()
+
+    cursor.close()
+    connection.close()
 
 # Function to query specific table
+@st.cache_data(ttl=600)
 def query_table(table_name):
-    data = supabase.table(table_name).select("*").execute()
-    df = pd.DataFrame(data.data)
-    return df
+    connection = psycopg2.connect(
+        host=st.secrets["connections"]["supabase"]["host"],
+        user=st.secrets["connections"]["supabase"]["user"],
+        password=st.secrets["connections"]["supabase"]["password"],
+        database=st.secrets["connections"]["supabase"]["database"],
+        port=st.secrets["connections"]["supabase"]["port"]
+    )
+    cursor = connection.cursor()
+
+    query = f"SELECT * FROM {table_name}"
+    cursor.execute(query)
+    
+    result = cursor.fetchall()
+    columns = [desc[0] for desc in cursor.description]
+    
+    cursor.close()
+    connection.close()
+    
+    return [dict(zip(columns, row)) for row in result]
